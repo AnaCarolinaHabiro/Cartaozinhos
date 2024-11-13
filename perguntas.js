56d1337491672fdf57b272bdd22796dc350d094c
Ana Carolina Habiro <00001103491350sp@al.educacao.sp.gov.br>
2024-11-13 11:32:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Anime',
+    'Qual personagem usa três espadas?',
+    ' Roronoa Zoro'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a causa da explosão do núcleo do reator da usina nuclear de Chernobyl"?',
+    ' Ocorreu durante um teste de segurança mal executado, os operadores desativaram sistemas de segurança, e levou a uma reação incapaz de ser controlada, resultando na explosão radioativa.'
+)
+
+criaCartao(
+    'Anime',
+    'Quem diz : "Está tudo bem, sabe porque? porque eu cheguei!"?',
+    ' All Might.'
+)
+
+criaCartao(
+    'Disney',
+    'Quem é o vilão que quis se tornar o mocinho?',
+    'Detona Ralph.'
+)
+
+criaCartao(
+    'Física',
+    'Qual partícula radioativa atravessa o corpo humano?',
+    'Gama.'
+)
+
+criaCartao(
+    'My little pony',
+    'Qual é a função da twilight sparkle?',
+    'Princesa da amizade.'
+)
